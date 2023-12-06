c23eeecfdf545db0beb66cdb2b9e20848dafbdd8
Nilam <shahnilam812@gmail.com>
2023-12-06 19:48:45 +0530

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Nilam Shah on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
